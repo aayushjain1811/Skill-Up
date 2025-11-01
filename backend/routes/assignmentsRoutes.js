95be4375858b1bafe3f2403431dc8c2edcc2772e
Ayush Jain <aayushjain1811@gmail.com>
2025-11-01 16:16:19 +0530

    Deleted a few backend files, updated a little UI

--- backend/routes/assignmentsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAssignments } from "../controllers/assignmentsController.js";
-
-const router = express.Router();
-
-// GET /api/resources/assignments/:courseId
-router.get("/:courseId", getAssignments);
-
-export default router;
--- backend/routes/notesRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getNotes } from "../controllers/notesController.js";
-
-const router = express.Router();
-
-// GET /api/resources/notes/:courseId
-router.get("/:courseId", getNotes);
-
-export default router;
